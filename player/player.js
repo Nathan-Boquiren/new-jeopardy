826d2a714b74bb68eb9e5e83a001a1e49b1d18d7
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-12-15 16:15:55 -0500

    player stuff

--- player/player.js
+++ player/player.js
@@ -1,3 +1,32 @@
 const cl = console.log;
 
 const btn = document.getElementById("btn");
+const inner = btn.querySelector(".btn-inner");
+
+btn.addEventListener("pointerdown", (e) => {
+  animateRipple(e.x, e.y);
+  playSound();
+});
+
+btn.addEventListener("keypress", (e) => {
+  if (e.key !== "Enter") return;
+  playSound();
+});
+
+function animateRipple(x, y) {
+  const ripple = document.createElement("div");
+  ripple.className = "ripple";
+  inner.append(ripple);
+  const xOff = x - inner.getBoundingClientRect().left;
+  const yOff = y - inner.getBoundingClientRect().top;
+  ripple.style.setProperty("--xOff", `${xOff}px`);
+  ripple.style.setProperty("--yOff", `${yOff}px`);
+  setTimeout(() => {
+    ripple.remove();
+  }, 500);
+}
+
+function playSound() {
+  const sound = new Audio("../assets/sfx/duolingo-sfx.mp3");
+  sound.play();
+}
